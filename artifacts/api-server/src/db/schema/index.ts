929803ac54099eb4ae2e517bf629e957f1501d48
mattiatekken2-hash <mattiatekken2@gmail.com>
2026-05-16 02:29:01 +0200

    Update index.ts

--- artifacts/api-server/src/db/schema/index.ts
+++ artifacts/api-server/src/db/schema/index.ts
@@ -1,8 +1,9 @@
-export * from "./users";
-export * from "./settings";
-export * from "./lottery";
-export * from "./labRanking";
 export * from "./chat";
-export * from "./roomInvites";
-export * from "./redeemCodes";
 export * from "./history";
+export * from "./labRanking";
+export * from "./lottery";
+export * from "./redeemCodes";
+export * from "./roomInvites";
+export * from "./settings";
+export * from "./users";
+
